dffc9e61cda1a8515268d7a07de754a3621345b0
Andreea Lucau <andreea.lucau@gmail.com>
2015-08-24 00:06:19 +0100

    Add lint as part of build

--- setup.py
+++ setup.py
@@ -1,4 +1,17 @@
+import distutils.cmd
+
+
+import setuptools.command.build_py
 from setuptools import setup, find_packages
+
+
+class BuildWithLintCommand(setuptools.command.build_py.build_py):
+    """Custom build command."""
+
+    def run(self):
+        self.run_command('lint')
+        setuptools.command.build_py.build_py.run(self)
+
 
 setup(
     name="hello",
@@ -20,4 +33,7 @@ setup(
         "setuptools-lint",
         "Sphinx"
     ],
+    cmdclass={
+        'build_py': BuildWithLintCommand,
+    },
 )
